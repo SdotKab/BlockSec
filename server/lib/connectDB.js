c20c54fb8bf22fc66a306761cce7910f133ae61e
Sama-E <112719147+Sama-E@users.noreply.github.com>
2025-01-12 16:52:51 -0500

    DB connected

---
+++ server/lib/connectDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import 'dotenv/config'
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("MongoDB is connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default connectDB;
