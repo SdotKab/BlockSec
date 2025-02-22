8340571e6a3ee027f95f893c53241c69909f4d6b
Sama-E <112719147+Sama-E@users.noreply.github.com>
2025-02-22 14:56:29 -0500

    erase dotenv/config

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -14,7 +14,6 @@ const router = express.Router();
 router.get("/upload-auth", uploadAuth);
 
 router.get("/", getPosts);
-// router.get("/:slug", getPost);
 router.get("/:slug", increaseVisit, getPost);
 router.post("/", createPost);
 router.delete("/:id", deletePost);
